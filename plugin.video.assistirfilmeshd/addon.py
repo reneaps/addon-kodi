f0ae5322b4d3711fe4188cf24ca4e641f5de9569
reneaps <reneaps@ig.com.br>
2016-11-09 14:42:15 -0200

    Update addon.py

--- plugin.video.assistirfilmeshd/addon.py
+++ plugin.video.assistirfilmeshd/addon.py
@@ -318,7 +318,7 @@ def player(name,url,iconimage):
 		listitem.setProperty('IsPlayable', 'true')
 		playlist.add(url2Play,listitem)
 
-		xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+		xbmcPlayer = xbmc.Player()
 		xbmcPlayer.play(playlist)
 		
 		mensagemprogresso.update(100)
@@ -429,7 +429,7 @@ def player_series(name,url,iconimage):
 		listitem.setProperty('IsPlayable', 'true')
 		playlist.add(url2Play,listitem)
 
-		xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+		xbmcPlayer = xbmc.Player()
 		xbmcPlayer.play(playlist)
 
 		mensagemprogresso.update(100)
