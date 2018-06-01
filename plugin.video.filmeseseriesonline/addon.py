f7122fdfda2b690c2d04a4578a1e385590c19a1e
reneaps <reneaps@ig.com.br>
2018-06-01 08:59:19 -0300

    Update filmeseseriesonline

--- plugin.video.filmeseseriesonline/addon.py
+++ plugin.video.filmeseseriesonline/addon.py
@@ -52,7 +52,7 @@ def menuPrincipal():
 		addDir('Lançamentos'				, base + '/filmes/lancamentos/' 	, 20, artfolder + 'ultimos.png')
 		addDir('Filmes em HD'				, base + '/filmes/filmes-hd/' 		, 20, artfolder + 'filmes.png')
 		addDir('Filmes Dublados'			, base + '/?s=dublado&tipo=video' 	, 20, artfolder + 'filmes.png')
-		addDir('Series'						, base + '/series-hd/'				, 25, artfolder + 'series.png')
+		addDir('Series'						, base + '/series/'					, 25, artfolder + 'series.png')
 		addDir('Pesquisa Series'			, '--'                           	, 30, artfolder + 'pesquisa.png')
 		addDir('Pesquisa Filmes'			, '--'                           	, 35, artfolder + 'pesquisa.png')
 		addDir('Configurações'				, base                             , 999, artfolder + 'config.png', 1, False)
@@ -382,7 +382,7 @@ def player(name,url,iconimage):
 
 		elif 'stream' in urlVideo :
 				fxID = urlVideo.split('=')[1]
-				urlVideo = 'http://estream.to/embed-%s.html' % fxID	
+				urlVideo = 'https://estream.to/embed-%s.html' % fxID
 				
 		elif 'streamango' in urlVideo :
 				fxID = urlVideo.split('=')[1]
