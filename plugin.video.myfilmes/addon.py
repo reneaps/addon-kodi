f46cb34d086541e16a54cbf318ca375633b9fc74
reneaps <reneaps@ig.com.br>
2018-06-28 10:59:05 -0300

    Updates

---
+++ plugin.video.myfilmes/addon.py
@@ -0,0 +1,31 @@
+#####################################################################
+#####################################################################
+# -*- coding: UTF-8 -*-
+#####################################################################
+# Addon : plugin.video.filmes
+# By AddonReneSilva - 28/06/2018
+# Atualizado (1.0.0) - 28/06/2018
+#####################################################################
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int (sys.argv [1])
+
+def pesquisa():
+		keyb = xbmc.Keyboard('', 'Entre com URL:')
+		keyb.doModal()
+
+		if (keyb.isConfirmed()):
+				texto	 = keyb.getText()
+				pesquisa = urllib.quote(texto)
+				url		 = str(pesquisa)
+		return url
+				
+xbmcplugin.setContent (addon_handle, 'movies')
+
+url = pesquisa()
+li = xbmcgui.ListItem ('Meu primeiro vídeo!', iconImage = 'DefaultVideo.png')
+xbmcplugin.addDirectoryItem (manipular = addon_handle, url = url, listitem = li)
+
+xbmcplugin.endOfDirectory (addon_handle)
